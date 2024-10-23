7fbd35e44896b892ab490cde80a7e087c06590cf
o.o <6whiteowl6@gmail.com>
2024-10-23 12:09:12 +0900

    [feat] 오브젝트 풀 구현

--- Assets/Scripts/QuestManager.cs
+++ Assets/Scripts/QuestManager.cs
@@ -17,11 +17,14 @@ public class QuestManager : MonoBehaviour
                 return instance;
             }
             // QuestManager 타입 가진 컴포넌트 찾아 대입
-            instance = FindObjectOfType(typeof(QuestManager)) as QuestManager;
+            instance = FindObjectOfType<QuestManager>();
+            // instance가 null이면
+            // (현재 씬에 QuestManager가 없으면)
             if (instance == null)
             {
-                // instance가 null이면 새로운 게임오브젝트 생성
-                instance = new QuestManager();
+                // 새로운 게임오브젝트 생성
+                GameObject go = new GameObject("QuestManager");
+                instance = go.AddComponent<QuestManager>();
             }
             return instance;
         }
@@ -30,10 +33,11 @@ public class QuestManager : MonoBehaviour
     // [구현사항 3] 인스턴스 검사 로직
     private void Awake()
     {
-        // 인스턴스가 null이 아니면
-        if (instance != null)
+        // 인스턴스가 null이 아니고
+        // (이미)존재하는 인스턴스가 현재 생성된 인스턴스가 아닌 경우
+        if (instance != null && instance != this)
         {
-            Destroy(instance);  // 생성된 인스턴스 파괴
+            Destroy(gameObject);  // 생성된 인스턴스 파괴
             return;
         }
         // 인스턴스가 null이면 현재 생성된 것으로 유지
